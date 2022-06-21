ca1c33d9cf4f71497b574e2a20ebab00fcc3169b
Jan Otto <j.otto@bvrn.de>
2022-06-21 19:35:40 +0200

    edited schemas

---
+++ bvrnapi/schemas/__init__.py
@@ -0,0 +1,3 @@
+from .association import Association, AssociationCreate, AssociationInDB, AssociationUpdate
+from .problem import Problem, ProblemCreate, ProblemInDB, ProblemUpdate, ProblemType
+from .additional import Representative
